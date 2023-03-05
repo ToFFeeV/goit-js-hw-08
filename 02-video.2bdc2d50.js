0fbc54d2a7dfa2d5504c4bcd34984cad0109de58
ToFFeeV <103739251+ToFFeeV@users.noreply.github.com>
2023-03-05 21:03:13 +0000

    Deploying to gh-pages from @ ToFFeeV/goit-js-hw-08@2cea7720ab05373c7f4e747fd29d87c28706bc4e 🚀

--- 02-video.2bdc2d50.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("iframe"),t=new Player(e);t.on("timeupdate",throttle((function(e){localStorage.setItem("videoplayer-current-time",e.seconds)}),1e3));const n=Number(localStorage.getItem("videoplayer-current-time"));t.setCurrentTime(n).then((function(e){})).catch((function(e){e.name}));
-//# sourceMappingURL=02-video.2bdc2d50.js.map
--- 02-video.a46ccb1a.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("iframe"),t=new Player(e);t.on("timeupdate",throttle((function(e){localStorage.setItem("videoplayer-current-time",e.seconds)}),1e3));var r=Number(localStorage.getItem("videoplayer-current-time"));t.setCurrentTime(r).then((function(e){})).catch((function(e){e.name}))}();
-//# sourceMappingURL=02-video.a46ccb1a.js.map
