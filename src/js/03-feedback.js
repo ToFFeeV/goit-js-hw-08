345f400e21482750d457a1ffff0747e821fbd5d2
ToFFeeV <khmiluvkiyv.ku@gmail.com>
2023-03-05 22:33:15 +0200

    Task 03 done.

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
 import throttle from 'lodash.throttle';
+
+const LOCAL_KEY = 'feedback-form-state';
+const formData = JSON.parse(localStorage.getItem(LOCAL_KEY)) || {};
+
+form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(onTextareaInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+populateTextarea();
+
+function onTextareaInput(evt) {
+    formData[evt.target.name] = evt.target.value;
+    localStorage.setItem(LOCAL_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    evt.currentTarget.reset();
+
+    localStorage.removeItem(LOCAL_KEY);
+    console.log(formData);
+}
+
+function populateTextarea() {
+    if (formData) {
+    let { email, message } = form.elements;
+    email.value = formData.email || '';
+    message.value = formData.message || '';
+    }
+}
